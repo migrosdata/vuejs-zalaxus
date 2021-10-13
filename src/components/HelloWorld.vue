435e66a24137a86715edb46a59c1a0ab0ee6a120
migrosdata <antoine.induni@gmail.com>
2021-10-13 21:39:51 +0200

    Fix typo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Zalakus AI App
+          Welcome to Zalaxus AI App
         </h1>
       </v-col>
 
