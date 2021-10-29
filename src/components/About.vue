7b359085dd7a91698e4f0f92e8c546e2fe453a60
migrosdata <antoine.induni@gmail.com>
2021-10-29 06:02:11 +0200

    Add components

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,6 +2,19 @@
  <v-container>
      <v-card>
         <v-card-title>About</v-card-title>
+        <v-card-text>
+            Hello
+              <v-img
+                height="300"
+                width="300"
+                src="https://robohash.org/1.png"
+                ></v-img>
+                              <v-img
+                height="300"
+                width="300"
+                src="https://robohash.org/2.png"
+                ></v-img>
+        </v-card-text>
      </v-card>
   </v-container>
 </template>
