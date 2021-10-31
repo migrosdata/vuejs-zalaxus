3436d5ccf4f2f15d1b8d8b4dff034a03663003cd
migrosdata <antoine.induni@gmail.com>
2021-10-31 07:40:53 +0100

    Add Spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-img
+        :src="require('../assets/bikeSpinner.gif')"
+    />
+</template>
+
+<script>
+export default {
+    name: 'BikeSpinner',
+}
+</script>
+
+<style>
+
+</style>
