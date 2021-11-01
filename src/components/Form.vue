09cfb125e004df2d3711227fe6456174bd82511a
migrosdata <antoine.induni@gmail.com>
2021-11-01 07:59:42 +0100

    Move form in form component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,27 +1,152 @@
-<i18n>
-{
-  "en": {
-    "hello": "hello world!"
-  },
-  "ja": {
-    "hello": "こんにちは、世界！"
-  }
-}
-</i18n>
 <template>
-    <p>message: {{ $t('hello') }}</p>
+        <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-select
+              :items="['North America', 'Europe', 'Pacific']"
+              :rules="[v => !!v || 'Region is required']"
+              filled
+              label="Region"
+            ></v-select>  
+            <v-text-field
+              v-model="age"
+              :rules="ageRules"
+              label="Age"
+              type="number"
+              min="0"
+              step="1"
+              required
+            ></v-text-field>        
+            <v-select
+              :items="['Single', 'Married']"
+              :rules="[v => !!v || 'Marital status is required']"              
+              filled
+              label="Marital Status"
+            ></v-select>              
+            <v-select
+              :items="['F', 'M']"
+              :rules="[v => !!v || 'Gender is required']"
+              filled
+              label="Gender"
+            ></v-select>               
+            <v-text-field
+              v-model="yearlyIncome"
+              :rules="yearlyIncome"
+              label="Yearly Income"
+              required
+              type="number"
+            ></v-text-field>    
+            <v-text-field
+              v-model="totalChildren"
+              :rules="totalChildren"
+              label="Total Children"
+              required
+              type="number"
+            ></v-text-field>      
+            <v-text-field
+              v-model="numberChildrenAtHome"
+              :rules="numberChildrenAtHome"
+              label="Number children at home"
+              required
+              type="number"
+            ></v-text-field>     
+            <v-select
+              :items="['Partial High School', 'High School', 'Partial College', 'Bachelors', 'Graduate Degree']"
+              :rules="[v => !!v || 'Education is required']"
+              filled
+              label="Education"
+            ></v-select>                     
+            <v-text-field
+              v-model="occupation"
+              :rules="occupation"
+              label="Occupation"
+              required
+            ></v-text-field>     
+            <v-checkbox
+              v-model="houseOwner"
+              :error-messages="errors"
+              value="1"
+              label="House Owner"
+              type="checkbox"
+              required
+            ></v-checkbox> 
+            <v-text-field
+              v-model="numberCarsOwned"
+              :rules="numberCarsOwned"
+              label="# cars Owned"
+              required
+              type="number"
+              min="0"
+            ></v-text-field>
+            <v-slider
+              v-model="numberCarsOwned"
+              :rules="numberCarsOwned"
+              color="orange"
+              label="# cars Owned"
+              hint="Be honest"
+              min="1"
+              max="10"
+              thumb-label
+            ></v-slider>
+            <v-select
+              :items="['0-1 Miles', '1-2 Miles', '2-5 Miles', '5-10 Miles', '10+ Miles']"
+              :rules="[v => !!v || 'Commute Distance is required']"
+              filled
+              label="Commute Distance"
+            ></v-select>    
+            <v-text-field
+              label="Height"              
+              suffix="cm"
+              type="number"
+            ></v-text-field>
+           <v-text-field
+              label="Weight"              
+              suffix="kg"
+              type="number"
+            ></v-text-field>
+            <v-select
+              :items="['Protestantism', 'Roman Catholicism', 'No religion']"
+              :rules="[v => !!v || 'Religion is required']"
+              filled
+              label="Religion"
+            ></v-select>               
+            <v-btn
+              color="success"
+              class="mr-4"
+              @click="validate"
+            >
+              Soumettre
+            </v-btn>
+        
+            <v-btn
+              color="error"
+              class="mr-4"
+            >
+              Reset</v-btn>
+        
+            <v-btn
+              color="warning"
+            >
+              Reset Validation
+            </v-btn>
+          </v-form>
 </template>
 
 <script>
 export default {
-  data () {
-    this.$i18n.locale = 'en';
-    return { locale: 'en' }
-  },
-  watch: {
-    locale (val) {
-      this.$i18n.locale = val
-    }
+  name: "Form",
+  data: () => ({
+    ageRules: [
+      v => v.length > 0 || 'Age is required',        
+      v => (v && Number.isInteger(Number(v))) || 'Age must be an integer',
+    ],
+  }),
+  methods: {
+    validate: function () {
+      this.$emit('submit');
+    },
   }
 }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -20,162 +20,50 @@
         class="mb-5"
         cols="12"
       >
-        <h2 class="headline font-weight-bold mb-3">
-        Saisir les données d'un prospect
-        </h2>
-        <v-form
-            ref="form"
-            v-model="valid"
-            lazy-validation
-          >
-            <v-select
-              :items="['North America', 'Europe', 'Pacific']"
-              :rules="[v => !!v || 'Region is required']"
-              filled
-              label="Region"
-            ></v-select>  
-            <v-text-field
-              v-model="age"
-              :rules="ageRules"
-              label="Age"
-              type="number"
-              min="0"
-              step="1"
-              required
-            ></v-text-field>        
-            <v-select
-              :items="['Single', 'Married']"
-              :rules="[v => !!v || 'Marital status is required']"              
-              filled
-              label="Marital Status"
-            ></v-select>              
-            <v-select
-              :items="['F', 'M']"
-              :rules="[v => !!v || 'Gender is required']"
-              filled
-              label="Gender"
-            ></v-select>               
-            <v-text-field
-              v-model="yearlyIncome"
-              :rules="yearlyIncome"
-              label="Yearly Income"
-              required
-              type="number"
-            ></v-text-field>    
-            <v-text-field
-              v-model="totalChildren"
-              :rules="totalChildren"
-              label="Total Children"
-              required
-              type="number"
-            ></v-text-field>      
-            <v-text-field
-              v-model="numberChildrenAtHome"
-              :rules="numberChildrenAtHome"
-              label="Number children at home"
-              required
-              type="number"
-            ></v-text-field>     
-            <v-select
-              :items="['Partial High School', 'High School', 'Partial College', 'Bachelors', 'Graduate Degree']"
-              :rules="[v => !!v || 'Education is required']"
-              filled
-              label="Education"
-            ></v-select>                     
-            <v-text-field
-              v-model="occupation"
-              :rules="occupation"
-              label="Occupation"
-              required
-            ></v-text-field>     
-            <v-checkbox
-              v-model="houseOwner"
-              :error-messages="errors"
-              value="1"
-              label="House Owner"
-              type="checkbox"
-              required
-            ></v-checkbox> 
-            <v-text-field
-              v-model="numberCarsOwned"
-              :rules="numberCarsOwned"
-              label="# cars Owned"
-              required
-              type="number"
-              min="0"
-            ></v-text-field>
-            <v-slider
-              v-model="numberCarsOwned"
-              :rules="numberCarsOwned"
-              color="orange"
-              label="# cars Owned"
-              hint="Be honest"
-              min="1"
-              max="10"
-              thumb-label
-            ></v-slider>
-            <v-select
-              :items="['0-1 Miles', '1-2 Miles', '2-5 Miles', '5-10 Miles', '10+ Miles']"
-              :rules="[v => !!v || 'Commute Distance is required']"
-              filled
-              label="Commute Distance"
-            ></v-select>    
-            <v-text-field
-              label="Height"              
-              suffix="cm"
-              type="number"
-            ></v-text-field>
-           <v-text-field
-              label="Weight"              
-              suffix="kg"
-              type="number"
-            ></v-text-field>
-            <v-select
-              :items="['Protestantism', 'Roman Catholicism', 'No religion']"
-              :rules="[v => !!v || 'Religion is required']"
-              filled
-              label="Religion"
-            ></v-select>               
-            <v-btn
-              color="success"
-              class="mr-4"
-              @click="validate"
-            >
-              Soumettre
-            </v-btn>
-        
-            <v-btn
-              color="error"
-              class="mr-4"
-              @click="reset"
-            >
-              Reset</v-btn>
-        
-            <v-btn
-              color="warning"
-              @click="resetValidation"
-            >
-              Reset Validation
-            </v-btn>
-          </v-form>
-        </v-col>
+        <zalaxus-form v-if="showForm" v-on:submit="submitHandler"></zalaxus-form>
+        <error v-if="showError"></error>
+        <result v-if="showResult"></result>
+        <spinner v-if="showSpinner"></spinner>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+  import Error from "./Error.vue";
+  import Result from "./Result.vue";
+  import Spinner from "./Spinner.vue";
+  import ZalaxusForm from "./Form.vue";
   export default {
     name: 'Home',
+    components: {
+      Error,
+      Spinner,
+      Result,
+      ZalaxusForm,      
+    },
     data: () => ({
-      ageRules: [
-        v => v.length > 0 || 'Age is required',        
-        v => (v && Number.isInteger(Number(v))) || 'Age must be an integer',
-      ],
+      showError: false,
+      showForm: true,
+      showResult: false,
+      showSpinner: false,
     }),
     methods: {
-      validate () {
-        this.$refs.form.validate()
-      },
+      submitHandler() {
+        this.showForm = false;
+        this.showSpinner = true;
+        this.axios.post('https://dataiku.hes-so.ch:8080/public/api/v1/cheapflixani/movies/lookup', { 
+            "data" : {
+            }
+          }).then((response) => {
+            console.log(response.data)
+            this.showSpinner = true;            
+
+          }).catch(error => {
+            this.showError = true;
+            console.log(error);
+          });
+      }
     }
   }
 </script>
