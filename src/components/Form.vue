5e3b1f840cea285e39628a5efbc2ec3611356bb1
migrosdata <antoine.induni@gmail.com>
2021-11-03 17:40:04 +0100

    Add v-model and submit method

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,13 +5,13 @@
             lazy-validation
           >
             <v-select
+              v-model="form.Region"
               :items="['North America', 'Europe', 'Pacific']"
               :rules="[v => !!v || 'Region is required']"
-              filled
               label="Region"
             ></v-select>  
             <v-text-field
-              v-model="age"
+              v-model.number="form.Age"
               :rules="ageRules"
               label="Age"
               type="number"
@@ -20,52 +20,55 @@
               required
             ></v-text-field>        
             <v-select
+              v-model="form.MaritalStatus"
               :items="['Single', 'Married']"
               :rules="[v => !!v || 'Marital status is required']"              
-              filled
               label="Marital Status"
             ></v-select>              
             <v-select
+              v-model="form.Gender"
               :items="['F', 'M']"
               :rules="[v => !!v || 'Gender is required']"
-              filled
               label="Gender"
             ></v-select>               
-            <v-text-field
-              v-model="yearlyIncome"
+            <v-text-field            
+              v-model.number="form.YearlyIncome"
               :rules="yearlyIncome"
               label="Yearly Income"
               required
               type="number"
+              min="0"
             ></v-text-field>    
             <v-text-field
-              v-model="totalChildren"
+              v-model.number="form.TotalChildren"
               :rules="totalChildren"
               label="Total Children"
               required
               type="number"
+              min="0"
             ></v-text-field>      
             <v-text-field
-              v-model="numberChildrenAtHome"
+              v-model.number="form.NumberChildrenAtHome"
               :rules="numberChildrenAtHome"
               label="Number children at home"
               required
               type="number"
+              min="0"
             ></v-text-field>     
             <v-select
+              v-model="form.EnglishEducation"
               :items="['Partial High School', 'High School', 'Partial College', 'Bachelors', 'Graduate Degree']"
               :rules="[v => !!v || 'Education is required']"
-              filled
               label="Education"
             ></v-select>                     
             <v-text-field
-              v-model="occupation"
+              v-model="form.EnglishOccupation"
               :rules="occupation"
               label="Occupation"
               required
             ></v-text-field>     
             <v-checkbox
-              v-model="houseOwner"
+              v-model.number="form.HouseOwnerFlag"
               :error-messages="errors"
               value="1"
               label="House Owner"
@@ -73,43 +76,37 @@
               required
             ></v-checkbox> 
             <v-text-field
-              v-model="numberCarsOwned"
+              v-model.number="form.NumberCarsOwned"
               :rules="numberCarsOwned"
               label="# cars Owned"
               required
               type="number"
               min="0"
             ></v-text-field>
-            <v-slider
-              v-model="numberCarsOwned"
-              :rules="numberCarsOwned"
-              color="orange"
-              label="# cars Owned"
-              hint="Be honest"
-              min="1"
-              max="10"
-              thumb-label
-            ></v-slider>
             <v-select
+              v-model="form.CommuteDistance"
               :items="['0-1 Miles', '1-2 Miles', '2-5 Miles', '5-10 Miles', '10+ Miles']"
               :rules="[v => !!v || 'Commute Distance is required']"
-              filled
               label="Commute Distance"
             ></v-select>    
             <v-text-field
+              v-model.number="form.Height"
               label="Height"              
               suffix="cm"
               type="number"
+              min="0"
             ></v-text-field>
            <v-text-field
+              v-model.number="form.Weight"
               label="Weight"              
               suffix="kg"
               type="number"
+              min="0"
             ></v-text-field>
             <v-select
+              v-model="form.Religion"
               :items="['Protestantism', 'Roman Catholicism', 'No religion']"
               :rules="[v => !!v || 'Religion is required']"
-              filled
               label="Religion"
             ></v-select>               
             <v-btn
@@ -125,12 +122,6 @@
               class="mr-4"
             >
               Reset</v-btn>
-        
-            <v-btn
-              color="warning"
-            >
-              Reset Validation
-            </v-btn>
           </v-form>
 </template>
 
@@ -138,14 +129,16 @@
 export default {
   name: "Form",
   data: () => ({
+    form: {
+    },
     ageRules: [
-      v => v.length > 0 || 'Age is required',        
+      v => v > 0 || 'Age is required',        
       v => (v && Number.isInteger(Number(v))) || 'Age must be an integer',
     ],
   }),
   methods: {
     validate: function () {
-      this.$emit('submit');
+      this.$emit('submit', this.form);
     },
   }
 }
