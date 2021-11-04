3ffa752a9c7f94b7e67d3fe6022b771890fe8ab5
migrosdata <antoine.induni@gmail.com>
2021-11-04 21:42:10 +0100

    Add Error Cat

--- src/components/Error.vue
+++ src/components/Error.vue
@@ -1,7 +1,16 @@
 <template>
-    <v-img
-        :src="require('../assets/Error.gif')"
-    />
+    <v-row class="text-center">
+        <v-col cols="12">
+            <h1 class="display-2 font-weight-bold mb-3">
+                Error
+            </h1>
+            <v-img
+                height="300"
+                contain
+                :src="require('../assets/Error.gif')"
+            />
+        </v-col>
+    </v-row>
 </template>
 
 <script>
